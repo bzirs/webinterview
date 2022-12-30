1edd56719ff80172e92397ceee7b10bba1659948
bzirs <bzirs@outlook.com>
2022-12-30 19:59:42 +0800

    feat: ✨ 修改文件配置

--- src/.vuepress/config.ts
+++ src/.vuepress/config.ts
@@ -7,8 +7,8 @@ export default defineUserConfig({
   locales: {
     "/": {
       lang: "zh-CN",
-      title: "文档演示",
-      description: "vuepress-theme-hope 的文档演示",
+      title: "",
+      description: "前端面试宝典整理",
     },
   },
 
